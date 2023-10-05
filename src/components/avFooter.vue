353a920264de65b088d8f1c6cda98a3b3a898334
francescomascellino <franesco.mascellino@gmail.com>
2023-10-05 12:03:55 +0200

    footer style

--- src/components/avFooter.vue
+++ src/components/avFooter.vue
@@ -2,37 +2,96 @@
     <footer>
         <section id="av-contacts">
 
-            <div class="container ">
+            <div class="container d-flex align-items-lg-center">
 
-                <div class="row align-items-center">
+                <div class="row align-items-start justify-content-between">
 
-                    <div class="col">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, enim iure dolorem minus a,
+                    <div class="col-3">
+                        <img src="../assets/img/logos/avada-drivers-logo-1x.png" alt="Avada Driving School"
+                            class="mb-3 av-ftrLogo">
+
+                        <p>Avadra Driving School is the right choice for experienced drivers looking for a refresher course,
+                            or the brand new driver wanting to pass the first time</p>
+
+                    </div>
+
+                    <div class="col-2 p-0">
+
+                        <h5 class="mb-3">CONTACT DETAILS</h5>
+
+                        <div class="d-flex mb-3 av-contactWrapper">
+                            <div class="ms-1 me-3 av-contactIco"><i class="fa-solid fa-house-chimney"></i></div>
+                            <div class="av-contact">
+                                <p>12345, Noth Main Street, New York, NY 555555</p>
+                            </div>
+                        </div>
+
+                        <div class="d-flex mb-3 av-contactWrapper">
+                            <div class="ms-1 me-3 av-contactIco"><i class="fa-solid fa-house-chimney"></i></div>
+                            <div class="av-contact">
+                                <p>12345, Noth Main Street</p>
+                            </div>
+                        </div>
+
+                        <div class="d-flex mb-3 av-contactWrapper">
+                            <div class="ms-1 me-3 av-contactIco"><i class="fa-solid fa-house-chimney"></i></div>
+                            <div class="av-contact">
+                                <p>12345, Noth Main Street</p>
+                            </div>
+                        </div>
+
+                        <div class="d-flex mb-3 av-contactWrapper">
+                            <div class="ms-1 me-3 av-contactIco"><i class="fa-solid fa-house-chimney"></i></div>
+                            <div class="av-contact">
+                                <p>12345, Noth Main Street</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="col-2 p-0">
+                        <h5 class="mb-3">COURSES</h5>
+
+                        <div class="d-flex mb-3 av-coursesLinktWrapper">
+                            <div class="ms-1 me-3 av-courseLinktIco"><i class="fa-regular fa-circle-right"></i></div>
+                            <div class="av-courseLink">
+                                <p>Pass Plus</p>
+                            </div>
+                        </div>
+
+                        <div class="d-flex mb-3 av-coursesLinktWrapper">
+                            <div class="ms-1 me-3 av-courseLinktIco"><i class="fa-regular fa-circle-right"></i></div>
+                            <div class="av-courseLink">
+                                <p>Pass Plus</p>
+                            </div>
+                        </div>
+
+                        <div class="d-flex mb-3 av-coursesLinktWrapper">
+                            <div class="ms-1 me-3 av-courseLinktIco"><i class="fa-regular fa-circle-right"></i></div>
+                            <div class="av-courseLink">
+                                <p>Pass Plus</p>
+                            </div>
+                        </div>
+
+                        <div class="d-flex mb-3 av-coursesLinktWrapper">
+                            <div class="ms-1 me-3 av-courseLinktIco"><i class="fa-regular fa-circle-right"></i></div>
+                            <div class="av-courseLink">
+                                <p>Pass Plus</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="col-3">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, enim iure dolorem minus
+                            a,
                             velit odio aspernatur quas quibusdam repudiandae aliquam? Veniam architecto neque earum
                             perspiciatis quidem nobis culpa sint?</p>
                     </div>
 
-                    <div class="col">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, enim iure dolorem minus a,
-                            velit odio aspernatur quas quibusdam repudiandae aliquam? Veniam architecto neque earum
-                            perspiciatis quidem nobis culpa sint?</p>
-                    </div>
-
-                    <div class="col">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, enim iure dolorem minus a,
-                            velit odio aspernatur quas quibusdam repudiandae aliquam? Veniam architecto neque earum
-                            perspiciatis quidem nobis culpa sint?</p>
-                    </div>
-
-                    <div class="col">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, enim iure dolorem minus a,
-                            velit odio aspernatur quas quibusdam repudiandae aliquam? Veniam architecto neque earum
-                            perspiciatis quidem nobis culpa sint?</p>
-                    </div>
                 </div>
 
             </div>
-
         </section>
 
         <section id="av-legal" class="av-ftrBottom">
@@ -41,8 +100,21 @@
 
                 <div class="row align-items-center">
                     <div class="col d-flex flex-row justify-content-between">
-                        <p>Lorem ipsum dolor sit amet consectetur.</p>
-                        <p>Lorem ipsum dolor sit amet consectetur.</p>
+                        <p>© Copyright 2012 - 2023 | Avada Theme by <a href="#">ThemeFusion</a> | All Rights Reserved |
+                            Powered by
+                            <a href="#">WordPress</a> WordPress
+                        </p>
+
+                        <div class="av-ftrSocial">
+                            <a class="px-2" href=""><i class="fa-brands fa-facebook-f"></i></a>
+
+                            <a class="px-2" href=""><i class="fa-brands fa-twitter"></i></a>
+
+                            <a class="px-2" href=""><i class="fa-brands fa-youtube"></i></a>
+
+                            <a class="px-2" href=""><i class="fa-brands fa-instagram"></i></a>
+                        </div>
+
                     </div>
                 </div>
 
